42a278c410d60af65bd558bd4a0ea8edf80fac37
sg-jaehyeon <jh.lee@supergate.cc>
2025-07-17 00:13:19 +0900

    [Fix] alias_field 매크로 임시 비활성화

--- src/api/acnt.rs
+++ src/api/acnt.rs
@@ -56,22 +56,22 @@ define_api!(
     }
 );
 
-alias_field!(Ka10072Res, dt_stk_div_rlzt_pl, 일자별종목별실현손익);
+//alias_field!(Ka10072Res, dt_stk_div_rlzt_pl, 일자별종목별실현손익);
 
-alias_field!(Ka10072ResItem, stk_nm, 종목명);
-alias_field!(Ka10072ResItem, cntr_qty, 체결량);
-alias_field!(Ka10072ResItem, buy_uv, 매입단가);
-alias_field!(Ka10072ResItem, cntr_pric, 체결가);
-alias_field!(Ka10072ResItem, tdy_sel_pl, 당일매도손익);
-alias_field!(Ka10072ResItem, pl_rt, 손익률);
-alias_field!(Ka10072ResItem, stk_cd, 종목코드);
-alias_field!(Ka10072ResItem, tdy_trde_cmsn, 당일매매수수료);
-alias_field!(Ka10072ResItem, tdy_trde_tax, 당일매매세금);
-alias_field!(Ka10072ResItem, wthd_alowa, 인출가능금액);
-alias_field!(Ka10072ResItem, loan_dt, 대출일);
-alias_field!(Ka10072ResItem, crd_tp, 신용구분);
-alias_field!(Ka10072ResItem, stk_cd_1, 종목코드_1);
-alias_field!(Ka10072ResItem, tdy_sel_pl_1, 당일매도손익_1);
+//alias_field!(Ka10072ResItem, stk_nm, 종목명);
+//alias_field!(Ka10072ResItem, cntr_qty, 체결량);
+//alias_field!(Ka10072ResItem, buy_uv, 매입단가);
+//alias_field!(Ka10072ResItem, cntr_pric, 체결가);
+//alias_field!(Ka10072ResItem, tdy_sel_pl, 당일매도손익);
+//alias_field!(Ka10072ResItem, pl_rt, 손익률);
+//alias_field!(Ka10072ResItem, stk_cd, 종목코드);
+//alias_field!(Ka10072ResItem, tdy_trde_cmsn, 당일매매수수료);
+//alias_field!(Ka10072ResItem, tdy_trde_tax, 당일매매세금);
+//alias_field!(Ka10072ResItem, wthd_alowa, 인출가능금액);
+//alias_field!(Ka10072ResItem, loan_dt, 대출일);
+//alias_field!(Ka10072ResItem, crd_tp, 신용구분);
+//alias_field!(Ka10072ResItem, stk_cd_1, 종목코드_1);
+//alias_field!(Ka10072ResItem, tdy_sel_pl_1, 당일매도손익_1);
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ka10073ResItem {
@@ -128,22 +128,22 @@ define_api!(
     }
 );
 
-alias_field!(Ka10073Res, dt_stk_rlzt_pl, 일자별종목별실현손익);
+//alias_field!(Ka10073Res, dt_stk_rlzt_pl, 일자별종목별실현손익);
 
-alias_field!(Ka10073ResItem, dt, 일자);
-alias_field!(Ka10073ResItem, tdy_htssel_cmsn, 당일hts매도수수료);
-alias_field!(Ka10073ResItem, stk_nm, 종목명);
-alias_field!(Ka10073ResItem, cntr_qty, 체결량);
-alias_field!(Ka10073ResItem, buy_uv, 매입단가);
-alias_field!(Ka10073ResItem, cntr_pric, 체결가);
-alias_field!(Ka10073ResItem, tdy_sel_pl, 당일매도손익);
-alias_field!(Ka10073ResItem, pl_rt, 손익률);
-alias_field!(Ka10073ResItem, stk_cd, 종목코드);
-alias_field!(Ka10073ResItem, tdy_trde_cmsn, 당일매매수수료);
-alias_field!(Ka10073ResItem, tdy_trde_tax, 당일매매세금);
-alias_field!(Ka10073ResItem, wthd_alowa, 인출가능금액);
-alias_field!(Ka10073ResItem, loan_dt, 대출일);
-alias_field!(Ka10073ResItem, crd_tp, 신용구분);
+//alias_field!(Ka10073ResItem, dt, 일자);
+//alias_field!(Ka10073ResItem, tdy_htssel_cmsn, 당일hts매도수수료);
+//alias_field!(Ka10073ResItem, stk_nm, 종목명);
+//alias_field!(Ka10073ResItem, cntr_qty, 체결량);
+//alias_field!(Ka10073ResItem, buy_uv, 매입단가);
+//alias_field!(Ka10073ResItem, cntr_pric, 체결가);
+//alias_field!(Ka10073ResItem, tdy_sel_pl, 당일매도손익);
+//alias_field!(Ka10073ResItem, pl_rt, 손익률);
+//alias_field!(Ka10073ResItem, stk_cd, 종목코드);
+//alias_field!(Ka10073ResItem, tdy_trde_cmsn, 당일매매수수료);
+//alias_field!(Ka10073ResItem, tdy_trde_tax, 당일매매세금);
+//alias_field!(Ka10073ResItem, wthd_alowa, 인출가능금액);
+//alias_field!(Ka10073ResItem, loan_dt, 대출일);
+//alias_field!(Ka10073ResItem, crd_tp, 신용구분);
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ka10074ResItem {
@@ -196,19 +196,19 @@ define_api!(
     }
 );
 
-alias_field!(Ka10074Res, tot_buy_amt, 총매수금액);
-alias_field!(Ka10074Res, tot_sell_amt, 총매도금액);
-alias_field!(Ka10074Res, rlzt_pl, 실현손익);
-alias_field!(Ka10074Res, trde_cmsn, 매매수수료);
-alias_field!(Ka10074Res, trde_tax, 매매세금);
-alias_field!(Ka10074Res, dt_rlzt_pl, 일자별실현손익);
+//alias_field!(Ka10074Res, tot_buy_amt, 총매수금액);
+//alias_field!(Ka10074Res, tot_sell_amt, 총매도금액);
+//alias_field!(Ka10074Res, rlzt_pl, 실현손익);
+//alias_field!(Ka10074Res, trde_cmsn, 매매수수료);
+//alias_field!(Ka10074Res, trde_tax, 매매세금);
+//alias_field!(Ka10074Res, dt_rlzt_pl, 일자별실현손익);
 
-alias_field!(Ka10074ResItem, dt, 일자);
-alias_field!(Ka10074ResItem, buy_amt, 매수금액);
-alias_field!(Ka10074ResItem, sell_amt, 매도금액);
-alias_field!(Ka10074ResItem, tdy_sel_pl, 당일매도손익);
-alias_field!(Ka10074ResItem, tdy_trde_cmsn, 당일매매수수료);
-alias_field!(Ka10074ResItem, tdy_trde_tax, 당일매매세금);
+//alias_field!(Ka10074ResItem, dt, 일자);
+//alias_field!(Ka10074ResItem, buy_amt, 매수금액);
+//alias_field!(Ka10074ResItem, sell_amt, 매도금액);
+//alias_field!(Ka10074ResItem, tdy_sel_pl, 당일매도손익);
+//alias_field!(Ka10074ResItem, tdy_trde_cmsn, 당일매매수수료);
+//alias_field!(Ka10074ResItem, tdy_trde_tax, 당일매매세금);
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Ka10075ResItem {
@@ -282,36 +282,36 @@ define_api!(
     }
 );
 
-alias_field!(Ka10075Res, oso, 미체결);
+//alias_field!(Ka10075Res, oso, 미체결);
 
-alias_field!(Ka10075ResItem, acnt_no, 계좌번호);
-alias_field!(Ka10075ResItem, ord_no, 주문번호);
-alias_field!(Ka10075ResItem, mang_empno, 관리사번);
-alias_field!(Ka10075ResItem, stk_cd, 종목코드);
-alias_field!(Ka10075ResItem, tsk_tp, 업무구분);
-alias_field!(Ka10075ResItem, ord_stt, 주문상태);
-alias_field!(Ka10075ResItem, stk_nm, 종목명);
-alias_field!(Ka10075ResItem, ord_qty, 주문수량);
-alias_field!(Ka10075ResItem, ord_pric, 주문가격);
-alias_field!(Ka10075ResItem, oso_qty, 미체결수량);
-alias_field!(Ka10075ResItem, cntr_tot_amt, 체결누계금액);
-alias_field!(Ka10075ResItem, orig_ord_no, 원주문번호);
-alias_field!(Ka10075ResItem, io_tp_nm, 주문구분);
-alias_field!(Ka10075ResItem, trde_tp, 매매구분);
-alias_field!(Ka10075ResItem, tm, 시간);
-alias_field!(Ka10075ResItem, cntr_no, 체결번호);
-alias_field!(Ka10075ResItem, cntr_pric, 체결가);
-alias_field!(Ka10075ResItem, cntr_qty, 체결량);
-alias_field!(Ka10075ResItem, cur_prc, 현재가);
-alias_field!(Ka10075ResItem, sel_bid, 매도호가);
-alias_field!(Ka10075ResItem, buy_bid, 매수호가);
-alias_field!(Ka10075ResItem, unit_cntr_pric, 단위체결가);
-alias_field!(Ka10075ResItem, unit_cntr_qty, 단위체결량);
-alias_field!(Ka10075ResItem, tdy_trde_cmsn, 당일매매수수료);
-alias_field!(Ka10075ResItem, tdy_trde_tax, 당일매매세금);
-alias_field!(Ka10075ResItem, ind_invsr, 개인투자자);
-alias_field!(Ka10075ResItem, stex_tp, 거래소구분);
-alias_field!(Ka10075ResItem, stex_tp_txt, 거래소구분텍스트);
-alias_field!(Ka10075ResItem, sor_yn, SOR여부값);
-alias_field!(Ka10075ResItem, stop_pric, 스톱가);
+//alias_field!(Ka10075ResItem, acnt_no, 계좌번호);
+//alias_field!(Ka10075ResItem, ord_no, 주문번호);
+//alias_field!(Ka10075ResItem, mang_empno, 관리사번);
+//alias_field!(Ka10075ResItem, stk_cd, 종목코드);
+//alias_field!(Ka10075ResItem, tsk_tp, 업무구분);
+//alias_field!(Ka10075ResItem, ord_stt, 주문상태);
+//alias_field!(Ka10075ResItem, stk_nm, 종목명);
+//alias_field!(Ka10075ResItem, ord_qty, 주문수량);
+//alias_field!(Ka10075ResItem, ord_pric, 주문가격);
+//alias_field!(Ka10075ResItem, oso_qty, 미체결수량);
+//alias_field!(Ka10075ResItem, cntr_tot_amt, 체결누계금액);
+//alias_field!(Ka10075ResItem, orig_ord_no, 원주문번호);
+//alias_field!(Ka10075ResItem, io_tp_nm, 주문구분);
+//alias_field!(Ka10075ResItem, trde_tp, 매매구분);
+//alias_field!(Ka10075ResItem, tm, 시간);
+//alias_field!(Ka10075ResItem, cntr_no, 체결번호);
+//alias_field!(Ka10075ResItem, cntr_pric, 체결가);
+//alias_field!(Ka10075ResItem, cntr_qty, 체결량);
+//alias_field!(Ka10075ResItem, cur_prc, 현재가);
+//alias_field!(Ka10075ResItem, sel_bid, 매도호가);
+//alias_field!(Ka10075ResItem, buy_bid, 매수호가);
+//alias_field!(Ka10075ResItem, unit_cntr_pric, 단위체결가);
+//alias_field!(Ka10075ResItem, unit_cntr_qty, 단위체결량);
+//alias_field!(Ka10075ResItem, tdy_trde_cmsn, 당일매매수수료);
+//alias_field!(Ka10075ResItem, tdy_trde_tax, 당일매매세금);
+//alias_field!(Ka10075ResItem, ind_invsr, 개인투자자);
+//alias_field!(Ka10075ResItem, stex_tp, 거래소구분);
+//alias_field!(Ka10075ResItem, stex_tp_txt, 거래소구분텍스트);
+//alias_field!(Ka10075ResItem, sor_yn, SOR여부값);
+//alias_field!(Ka10075ResItem, stop_pric, 스톱가);
 
